90564a7940c4c053c3515ee16c5edbfd151b3336
Tanishq Uppal <tanishquppal220@gmail.com>
2026-05-31 00:29:49 +0530

    Add: string-encode-and-decode - submission-4

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-4.py
@@ -0,0 +1,27 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        res = ""
+        for i in strs:
+            temp =  f"{len(i)} "
+            for j in i:
+                temp+=chr(ord(j)+3)
+            res+=temp+" "
+        return res
+    def decode(self, s: str) -> List[str]:
+        s = s.strip()
+        if s == "":
+            return []
+        l = s.split(" ")
+        res = []
+        print(l)
+        for i in range(0,len(l),2):
+            print(l[i])
+            if int(l[i]) == 0:
+                res.append("") 
+                continue
+            temp = ""
+            for j in l[i+1]:
+                temp+=chr(ord(j)-3)
+            res.append(temp)
+        return res
